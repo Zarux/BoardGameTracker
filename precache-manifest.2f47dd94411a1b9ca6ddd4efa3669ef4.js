31ef144046401f377dce99ff799a55af154e1b91
Joakim Tangnes <joakimtangnes@gmail.com>
2019-03-19 21:53:37 +0100

    Updates

---
+++ precache-manifest.2f47dd94411a1b9ca6ddd4efa3669ef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68fd4160ae1919763c41",
+    "url": "/BoardGameTracker/static/css/main.95f07b9b.chunk.css"
+  },
+  {
+    "revision": "68fd4160ae1919763c41",
+    "url": "/BoardGameTracker/static/js/main.e31081c1.chunk.js"
+  },
+  {
+    "revision": "187003889d86bd0aa2b1",
+    "url": "/BoardGameTracker/static/js/runtime~main.1400071d.js"
+  },
+  {
+    "revision": "dd41752b21796ca45b7a",
+    "url": "/BoardGameTracker/static/js/2.fd7f6f54.chunk.js"
+  },
+  {
+    "revision": "a8fc925a2842f55c0999bd24f330996b",
+    "url": "/BoardGameTracker/index.html"
+  }
+];
